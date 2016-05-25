758a54bd3b0b04f75c3a5ed66423f1b9fc95969f
Philip Helger <philip@helger.com>
2016-05-25 16:55:11 +0200

    Added example Schematron validation

--- edifact/iso26025-schematron/src/main/java/eu/cen/tc434/edifact/MainApplySchematron.java
+++ edifact/iso26025-schematron/src/main/java/eu/cen/tc434/edifact/MainApplySchematron.java
@@ -1,9 +1,18 @@
 package eu.cen.tc434.edifact;
 
+import java.util.List;
+
+import org.oclc.purl.dsdl.svrl.SchematronOutputType;
 import org.unece.cefact.invoic.d14b.MINVOIC;
 
 import com.helger.commons.io.resource.FileSystemResource;
 import com.helger.commons.system.SystemProperties;
+import com.helger.schematron.ISchematronResource;
+import com.helger.schematron.SchematronHelper;
+import com.helger.schematron.pure.SchematronResourcePure;
+import com.helger.schematron.svrl.SVRLFailedAssert;
+import com.helger.schematron.svrl.SVRLHelper;
+import com.helger.schematron.svrl.SVRLWriter;
 
 public final class MainApplySchematron
 {
@@ -11,6 +20,7 @@ public final class MainApplySchematron
   {
     SystemProperties.setPropertyValue ("jdk.xml.maxOccurLimit", Integer.toString (9_999_999));
     final FileSystemResource aXML = new FileSystemResource ("src/main/resources/xml/invoic5.xml");
+    final FileSystemResource aSCH = new FileSystemResource ("src/main/resources/sch/example.sch");
 
     final InvoicReader aReader = new InvoicReader ();
     final MINVOIC aInvoic = aReader.read (aXML);
@@ -21,7 +31,24 @@ public final class MainApplySchematron
     else
     {
       System.out.println ("XML is valid according to XSD!");
-      // SchematronHelper.applySchematron (aSchematron, aXML);
+
+      final ISchematronResource aSchematron = new SchematronResourcePure (aSCH);
+      final SchematronOutputType aSOT = SchematronHelper.applySchematron (aSchematron, aXML);
+      if (aSOT == null)
+        System.out.println ("Seems like the Schematron has error - check the console log!");
+      else
+      {
+        final String sSVRL = SVRLWriter.createXMLString (aSOT);
+        final List <SVRLFailedAssert> aFailedAsserts = SVRLHelper.getAllFailedAssertions (aSOT);
+        if (aFailedAsserts.isEmpty ())
+        {
+          System.out.println ("XML complies to Schematron! SVRL:\n" + sSVRL);
+        }
+        else
+        {
+          System.err.println ("XML does not comply to Schematron! SVRL:\n" + sSVRL);
+        }
+      }
     }
   }
 }
---
+++ edifact/iso26025-schematron/src/main/resources/sch/example.sch
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<schema xmlns="http://purl.oclc.org/dsdl/schematron" queryBinding="xslt2">
+  <title>TC434 bound to ISO 26025</title>
+  <pattern id="TC434">
+    <rule context="/M_INVOIC/S_UNH/D_0062">
+      <assert test="string-length(text()) &lt; 5" flag="fatal" id="dummy">Something must be present</assert>
+    </rule>
+  </pattern>  
+</schema>
