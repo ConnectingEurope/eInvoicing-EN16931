0bf89c761268cd2e4890a8115a29441651ce1889
AndreasPvd <andreas.pelekies@validool.org>
2016-07-12 00:47:13 +0200

    Adding windows based validator using Philips Library, Jörgs Mapping Project and applying Yildirays batch calls

--- edifact/validator/validate_xsd.bat
+++ edifact/validator/validate_xsd.bat
@@ -1,4 +1,4 @@
 set XSD_PATH=%1
 set XML_PATH=%2
 
-java -jar xmlvalidator-0.1.jar -xml %XML_PATH% -xsd %XSD_PATH%
+java -jar xmlvalidator-1.0-SNAPSHOT-jar-with-dependencies.jar -xml %XML_PATH% -xsd %XSD_PATH%
--- edifact/validator/validate_xsd.sh
+++ edifact/validator/validate_xsd.sh
@@ -3,4 +3,4 @@
 XSD_PATH=$1
 XML_PATH=$2
 
-java -jar xmlvalidator-0.1.jar -xml "$XML_PATH" -xsd "$XSD_PATH"
+java -jar xmlvalidator-1.0-SNAPSHOT-jar-with-dependencies.jar -xml "$XML_PATH" -xsd "$XSD_PATH"
