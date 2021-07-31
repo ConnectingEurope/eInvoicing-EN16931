0f3c7c19a1ed8f5a5288d7b62a239616091c864a
Philip Helger <philip@helger.com>
2021-07-31 21:45:09 +0200

    Added possibilities to easily run tests locally

---
+++ run-tests.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+echo running UBL tests
+docker run --rm -it -v %CD%:/src difi/vefa-validator build -x -t -p ubl /src
+if errorlevel 1 goto error
+
+echo running CII tests
+docker run --rm -it -v %CD%:/src difi/vefa-validator build -x -t -p cii /src
+if errorlevel 1 goto error
+
+goto end
+
+:error
+echo An error occurred in validation
+pause
+
+:end
