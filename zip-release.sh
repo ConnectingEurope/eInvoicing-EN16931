23d14d0c3835047d2d5d02a7cee0a18a6f60cc15
Philip Helger <philip@helger.com>
2024-10-08 18:12:23 +0200

    Added release build script

---
+++ zip-release.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+ver=1.3.13
+
+#
+# CII
+#
+rm en16931-cii-$ver.zip
+cd cii
+zip -9 -r ../en16931-cii-$ver.zip schematron/* xslt/* examples/*
+cd ..
+
+#
+# UBL
+#
+rm en16931-ubl-$ver.zip
+cd ubl
+zip -9 -r ../en16931-ubl-$ver.zip schematron/* xslt/* examples/*
+cd ..
+
+echo "Successfully done"
